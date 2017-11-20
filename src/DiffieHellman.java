b81f7ec1b4b86be7bf941965811788c5c7a2c6b3
Miguel Angel Sanchez Cifo <miguelssanchez@gmail.com>
2017-11-20 21:14:58 +0100

    Code improvements

--- src/DiffieHellman.java
+++ src/DiffieHellman.java
@@ -2,82 +2,52 @@ import java.math.BigInteger;
 
 public class DiffieHellman {
 
-    private static BigInteger clavePrivada = new BigInteger(String.valueOf(497));
-
-    // Me pongo de acuerdo con estos valores con mi pareja
-    private static BigInteger p = new BigInteger(String.valueOf(852));
-    private static BigInteger n = new BigInteger(String.valueOf(749720981));
-
     public static void main(String[] argv) {
 
+        BigInteger clavePrivada = new BigInteger(String.valueOf(497));
+        BigInteger p = new BigInteger(String.valueOf(852));
+        BigInteger n = new BigInteger(String.valueOf(749720981));
+        BigInteger Z_Bob = new BigInteger(String.valueOf(445683894));
+
         System.out.println("Ejercicio 2\n");
-        fuerzaBruta(exponencialModular(p, clavePrivada, n), new BigInteger(String.valueOf(445683894)), p, n);
+        ataque(exponencialModular(p, clavePrivada, n), Z_Bob, p, n);
 
         System.out.println("\n\nEjercicio 3 - Fuerza Bruta\n");
-        fuerzaBruta(new BigInteger(String.valueOf(79525)), new BigInteger(String.valueOf(106243)), new BigInteger(String.valueOf(253)), new BigInteger(String.valueOf(131074)));
+        ataque(new BigInteger(String.valueOf(79525)), new BigInteger(String.valueOf(106243)), new BigInteger(String.valueOf(253)), new BigInteger(String.valueOf(131074)));
     }
 
-    private static BigInteger exponencialModular(BigInteger base, BigInteger exp, BigInteger mod) {
-        BigInteger result = BigInteger.ONE;
-        base = base.mod(mod);
+    private static BigInteger exponencialModular(BigInteger p, BigInteger clavePrivada, BigInteger n) {
+        BigInteger resultado = BigInteger.ONE;
+        p = p.mod(n);
 
-        for (int idx = 0; idx < exp.bitLength(); ++idx) {
-            if (exp.testBit(idx)) {
-                result = result.multiply(base).mod(mod);
+        for (int i = 0; i < clavePrivada.bitLength(); ++i) {
+            if (clavePrivada.testBit(i)) {
+                resultado = resultado.multiply(p).mod(n);
             }
-            base = base.multiply(base).mod(mod);
+            p = p.multiply(p).mod(n);
         }
-        return result;
+        return resultado;
     }
 
-    private static void fuerzaBruta(BigInteger A, BigInteger B, BigInteger b, BigInteger m) {
-
-        int i, j;
+    private static void ataque(BigInteger A, BigInteger B, BigInteger b, BigInteger m) {
         int clave_alice = 0;
         int clave_bob = 0;
-        boolean bob = false;
-        boolean alice = false;
 
-        for (i = 0; i <= m.intValue() - 1; i++) {
-
-            if (B.equals(exponencialModular(b, new BigInteger(String.valueOf(i)), m))) {
-                // Ya tengo el numero secreto de Bob
-                System.out.println("Clave secreta de Bob: " + i);
-                System.out.println("Z de Bob: " + exponencialModular(b, new BigInteger(String.valueOf(i)), m));
-                bob = true;
-                clave_bob = i;
-                break;
-            }
-
+        for (int i = 0; i <= m.intValue() - 1; i++) {
             if (A.equals(exponencialModular(b, new BigInteger(String.valueOf(i)), m))) {
-                // Ya tengo el numero secreto de Alice
                 System.out.println("Clave secreta de Alice: " + i);
                 System.out.println("Z de Alice: " + exponencialModular(b, new BigInteger(String.valueOf(i)), m));
-                alice = true;
                 clave_alice = i;
                 break;
             }
         }
 
-        if (bob) {
-            for (j = i; j <= m.intValue() - 1; j++) {
-                if (A.equals(exponencialModular(b, new BigInteger(String.valueOf(j)), m))) {
-                    System.out.println("Clave secreta de Alice: " + j);
-                    System.out.println("Z de Alice: " + exponencialModular(b, new BigInteger(String.valueOf(j)), m));
-                    clave_alice = j;
-                    break;
-                }
-            }
-        }
-
-        if (alice) {
-            for (j = i; j <= m.intValue() - 1; j++) {
-                if (B.equals(exponencialModular(b, new BigInteger(String.valueOf(j)), m))) {
-                    System.out.println("Clave secreta de Bob: " + j);
-                    System.out.println("Z de Bob: " + exponencialModular(b, new BigInteger(String.valueOf(j)), m));
-                    clave_bob = j;
-                    break;
-                }
+        for (int i = 0; i <= m.intValue() - 1; i++) {
+            if (B.equals(exponencialModular(b, new BigInteger(String.valueOf(i)), m))) {
+                System.out.println("Clave secreta de Bob: " + i);
+                System.out.println("Z de Bob: " + exponencialModular(b, new BigInteger(String.valueOf(i)), m));
+                clave_bob = i;
+                break;
             }
         }
 
